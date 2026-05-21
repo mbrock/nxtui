88e324f82fcccbf3faeb4ffe630b0eb023329132
Mikael Brockman <mikael@brockman.se>
2026-05-21 18:33:03 +0200

    Make ng runtime tracing opt-in

--- src-ng/nxt/rt/trace.hpp
+++ src-ng/nxt/rt/trace.hpp
@@ -1,11 +1,23 @@
 #pragma once
 
+#include <cstdlib>
 #include <iostream>
 #include <string_view>
 
 namespace nxt::rt {
 
-inline bool trace_enabled = true;
+inline bool trace_env_enabled() noexcept
+{
+    auto const * raw = std::getenv("NXT_RT_TRACE");
+    if (raw == nullptr)
+        return false;
+
+    auto value = std::string_view{raw};
+    return value == "1" || value == "true" || value == "yes"
+        || value == "on";
+}
+
+inline bool trace_enabled = trace_env_enabled();
 
 inline void trace(std::string_view message)
 {
